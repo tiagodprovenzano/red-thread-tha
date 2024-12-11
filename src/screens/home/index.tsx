6cc00a289f3c3d9a469bd94700522593f8f7ef2b
Tiago Dias Provenzano <tiagodprovenzano@gmail.com>
2024-12-11 07:27:28 -0300

    feat: add HomeScreen component with movie grid layout

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,21 @@
+import { Header } from "@/components/header/component";
+import { Movie } from "@/model";
+import "./style.css";
+import { MovieItem } from "@/components/movie-item";
+
+interface HomeScreenProps {
+  movies: Movie[];
+}
+
+export const HomeScreen = ({ movies }: HomeScreenProps) => {
+  return (
+    <div className="content-wrapper">
+      <Header title="Pop Movies" />
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieItem movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
